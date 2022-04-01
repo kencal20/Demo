616443b5e171cdb603ffe053b41d954cc2095dde
kencal20 <kenadjei20@gmail.com>
2022-04-01 12:07:11 +0000

    Home - starting

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import{View,Text} from 'react-native'
+const Home = () => {
+    return (
+      <View>
+          <Text>
+              Welcome to Home
+          </Text>
+      </View>
+    );
+}
+
+export default Home;
