5b974d3de609ee4f0dcb9db4fb2052be00a7acde
kencal20 <kenadjei20@gmail.com>
2022-04-01 12:08:01 +0000

    workers loan page update

---
+++ src/Workersloan.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+
+const Workersloan = () => {
+    return (
+        <View style={styles.container}>
+             <Text>
+             Welcome  to student loan , where we give the oppurtunity to students 
+             to continue their studies by loaning them money for their School which 
+             in turn pay back the money with low interest
+         </Text>
+        </View>
+    );
+}
+
+export default Workersloan;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
