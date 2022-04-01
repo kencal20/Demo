43dfba179d376d2d8c592fcbbf19f74156a38aa4
kencal20 <kenadjei20@gmail.com>
2022-04-01 12:06:43 +0000

    app js update

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
-import {  StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/Home';
+import Loan from './src/Loan';
+import Home from './src/Home'
 import Studentloan from './src/Studentloan';
 import Workersloan from './src/Workersloan';
-import Pensionloan from './src/Pensionloan'
+import Pensionloan from './src/Pensionloan';
+import Main from './src/components/Main'
 import React from 'react';
 
 
@@ -13,11 +15,20 @@ const Stack = createNativeStackNavigator()
 export default function App() {
   return (
     <NavigationContainer >
-      <Stack.Navigator >
-    <Stack.Screen name='Home' component={Home}/>
-    <Stack.Screen name='Studentloan' component={Studentloan} />
-    <Stack.Screen name='Workersloan' component={Workersloan} />
-    <Stack.Screen name='Pensionloan' component={Pensionloan} />
+
+      <Stack.Navigator
+
+      >
+        <Stack.Screen name='Main' component={Main}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name='Home' component={Home}
+        />
+        <Stack.Screen name='Studentloan' component={Studentloan} />
+        <Stack.Screen name='Workersloan' component={Workersloan} />
+        <Stack.Screen name='Pensionloan' component={Pensionloan} />
       </Stack.Navigator>
     </NavigationContainer>
   );
