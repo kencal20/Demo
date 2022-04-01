05891aaf95b811e69cfc85afe4c68f8f24ec2eca
kencal20 <kenadjei20@gmail.com>
2022-04-01 01:34:27 +0000

    app.js

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {  StyleSheet } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/Home';
+import Studentloan from './src/Studentloan';
+import Workersloan from './src/Workersloan';
+import Pensionloan from './src/Pensionloan'
+import React from 'react';
+
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer >
+      <Stack.Navigator >
+    <Stack.Screen name='Home' component={Home}/>
+    <Stack.Screen name='Studentloan' component={Studentloan} />
+    <Stack.Screen name='Workersloan' component={Workersloan} />
+    <Stack.Screen name='Pensionloan' component={Pensionloan} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
+
+
+const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+  }
+})
