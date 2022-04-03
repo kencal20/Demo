ed7ffb599371ba0b942d4be17cd2a8d3616776a8
kencal20 <kenadjei20@gmail.com>
2022-04-03 10:58:27 +0000

    bank js file creation

---
+++ src/Bank.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View,Text} from 'react-native'
+const Bank = () => {
+    return (
+    <View>
+<Text>
+    Bank
+</Text>
+    </View>
+    );
+}
+
+export default Bank;
