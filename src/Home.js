faf3de37094fb1e57f52adcbcf784565bf0a8eaa
kencal20 <kenadjei20@gmail.com>
2022-04-03 22:23:00 +0000

    home update

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet,Image  } from 'react-native';
-import { FontAwesome} from '@expo/vector-icons'
-import join from  "../assets/join.png"
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons'
+import join from "../assets/join.png"
 const Home = () => {
     return (
         <View style={styles.container}>
@@ -22,12 +22,12 @@ const Home = () => {
 
                 </Text>
                 <Image source={join} style={styles.image} />
-                </View>
-                <Text style={styles.link_ref}>
-                    click on  <FontAwesome name='money' size={20} /> button for
-                    the loan packages
-                </Text>
-            
+            </View>
+            <Text style={styles.link_ref}>
+                click on  <FontAwesome name='money' size={20} /> button for
+                the loan packages
+            </Text>
+
         </View>
     );
 }
@@ -37,42 +37,47 @@ export default Home;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginHorizontal: 20
+        backgroundColor: '#411cd9'
     },
 
     welcome_text: {
-        marginTop:40,
+        marginTop: 40,
         fontSize: 30,
         fontWeight: 'bold',
-        color:'orange'
-
+        color: 'orange',
+        marginHorizontal:30
     },
     About: {
-        marginVertical:50
+        marginVertical: 50,
+        marginHorizontal: 20
     }
     ,
 
     about_header: {
         fontSize: 20,
         fontWeight: 'bold',
-
+        color: 'white'
     },
 
     about_text: {
         marginVertical: 20,
         marginHorizontal: 10,
-        
-       
+        color: 'white',
+
+
+
     },
     image: {
         width: 200,
         height: 200,
         marginBottom: 40,
         alignSelf: 'center',
-      
+
     },
-    link_ref:{
-        fontSize:20
+    link_ref: {
+        fontSize: 20,
+        color: 'white',
+        marginHorizontal: 30
     }
 
 })
