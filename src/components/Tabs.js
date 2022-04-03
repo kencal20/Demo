8763ae96cb9a345c4c0eab5e7302dcc32bee6ab9
kencal20 <kenadjei20@gmail.com>
2022-04-03 11:01:34 +0000

    Tab File update

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -4,27 +4,51 @@ import { StyleSheet, View } from 'react-native'
 import Loan from "../Loan";
 import Home from "../Home";
 import { AntDesign, FontAwesome } from '@expo/vector-icons'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
 
 
 
 const Tab = createBottomTabNavigator();
+// const Tab = createMaterialBottomTabNavigator();
 
 const Tabs = () => {
     return (
         <Tab.Navigator
-        tabBarOptions={{
-            showLabel: false,
-        }}
-            
+            style={styles.shadow}
+            screenOptions={{
+                headerShown: false,
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    height: 60,
+                    position: 'absolute',
+                    bottom: 25,
+                    right: 16,
+                    left: 16,
+                    borderRadius: 15,
+                    backgroundColor: "dodgerblue",
+
+                }
+
+
+            }}
+        // initialRouteName="Home"
+        // activeColor="#f0edf6"
+        // inactiveColor="#3e2465"
+        // barStyle={{ backgroundColor: '#694fad' }}
+
 
         >
             <Tab.Screen name='Home' component={Home} options={{
+                        
+
                 tabBarIcon: ({ focused }) => (
                     <View>
                         <AntDesign name='home' size={30}
                         />
                     </View>
                 )
+
             }} />
 
 
@@ -41,7 +65,7 @@ const Tabs = () => {
 
     );
 }
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     shadow: {
         shadowOffset: {
             width: 0,
