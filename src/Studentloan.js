e6d86db9e9e621fe92c934668ddcdee77be124b5
kencal20 <kenadjei20@gmail.com>
2022-04-03 11:00:02 +0000

    studentloan file update

--- src/Studentloan.js
+++ src/Studentloan.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
-const Studentloan = () => {
+const Studentloan = ({ navigation }) => {
     return (
         <View style={styles.container}>
-         <Text>
-             Welcome  to student loan , where we give the oppurtunity to students 
-             to continue their studies by loaning them money for their School which 
-             in turn pay back the money with low interest
-         </Text>
-        </View>
+            <Text>
+                Welcome  to student loan , where we give the oppurtunity to students
+                to continue their studies by loaning them money for their School which
+                in turn pay back the money with low interest
+            </Text>
+            <TouchableOpacity 
+            onPress={() => {
+                navigation.navigate('Bank')
+              }} 
+              style={styles.button} >
+                    Next
+            </TouchableOpacity>
+        </View >
     );
 }
 
@@ -21,6 +28,12 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        fontSize: 40
+    },
+    button:{
+        backgroundColor:'dodgerblue',
+        borderRadius:20,
+        borderWidth:3,
         fontSize:40
     }
 })
