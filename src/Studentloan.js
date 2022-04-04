3dc8635e6c943fb3ba7b296408bd828246c516bd
kencal20 <kenadjei20@gmail.com>
2022-04-04 21:25:26 +0000

    Student loan update

--- src/Studentloan.js
+++ src/Studentloan.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-
+import { View, Text, StyleSheet, TouchableOpacity,Image } from 'react-native';
+import Student from '../assets/Student.jpg';
 const Studentloan = ({ navigation }) => {
     return (
         <View style={styles.container}>
-            <Text>
-                Welcome  to student loan , where we give the oppurtunity to students
-                to continue their studies by loaning them money for their School which
-                in turn pay back the money with low interest
-            </Text>
-            <TouchableOpacity
-                onPress={() => {
-                    navigation.navigate('Bank')
-                }}
-            >
-                <Text style={styles.button}>
-                    Next
+            <View >
+            <Image source={Student} style={styles.image} />
+            </View>
+                <Text style={styles.header}>
+                    Student Loan
                 </Text>
-            </TouchableOpacity>
-        </View >
+
+                <Text style={styles.subtext}>
+                    Welcome  to student loan , where we give the oppurtunity to students
+                    to continue their studies by loaning them money for their School which
+                    in turn pay back the money with low interest
+                </Text>
+                <View style={styles.buttons}>
+                    <View style={styles.button1}>
+                        <TouchableOpacity
+                            onPress={() => {
+                                navigation.navigate('Bank')
+                            }}
+                        >
+                            <Text style={styles.button}>
+                                Continue
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.button2}>
+                        <TouchableOpacity
+                            onPress={() => {
+                                navigation.navigate('Loan')
+                            }}
+                        >
+                            <Text style={styles.button}>
+                                Back
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View >
+      
     );
 }
 
@@ -27,15 +50,58 @@ export default Studentloan;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#364bba',
         alignItems: 'center',
         justifyContent: 'center',
-        fontSize: 40
+
+    },
+
+ 
+    image:{
+        width: 300,
+        height:300,
+        marginBottom: 40,
+        alignSelf: 'center',
+        borderRadius:200
+    },
+    header: {
+        color: 'white',
+        fontSize: 30,
+        marginVertical:30,
+        right:70
+
+    },
+    subtext: {
+        color: 'white',
+        marginHorizontal:30,
+    
+       
+    },
+    buttons: {
+        flexDirection: 'row',
+         marginVertical:20
     },
     button: {
+        fontSize: 20,
+        color: 'white'
+    },
+    button1: {
         backgroundColor: 'dodgerblue',
+        borderWidth: 0,
         borderRadius: 10,
-        borderWidth: 2,
-        fontSize: 30
+        height: 50,
+        width: 150,
+        alignItems: 'center',
+        justifyContent: 'center'
+
     }
+    ,
+    button2: {
+        fontSize: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginHorizontal: 20
+
+    }
+
 })
