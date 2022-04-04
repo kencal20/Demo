c77577a8fc76865736b602546503f9001dcfa4e8
kencal20 <kenadjei20@gmail.com>
2022-04-04 12:16:48 +0000

    follow .js update

--- src/follow.js
+++ src/follow.js
@@ -14,14 +14,14 @@ const Follow = () => {
                 <View style={styles.fb}>
                     <TouchableOpacity style={styles.Follow}>
                         <Text style={styles.follow_text}>
-                            Facebook
+                            <Entypo name='facebook' size={50} color='white' />
                         </Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.twitter}>
                     <TouchableOpacity style={styles.Follow}>
                         <Text style={styles.follow_text}>
-                            Twitter
+                            <Entypo name='twitter' size={50} color='white' />
                         </Text>
                     </TouchableOpacity>
                 </View>
@@ -30,7 +30,7 @@ const Follow = () => {
                 <View style={styles.Insta}>
                     <TouchableOpacity style={styles.Follow}>
                         <Text style={styles.follow_text}>
-                            Instagram
+                            <Entypo name='instagram' size={50} color='white' />
                         </Text>
                     </TouchableOpacity>
                 </View>
@@ -38,38 +38,38 @@ const Follow = () => {
                 <View style={styles.linkedin}>
                     <TouchableOpacity style={styles.Follow}>
                         <Text style={styles.follow_text}>
-                            Linkedin
+                            <Entypo name='linkedin' size={50} color='white' />
                         </Text>
                     </TouchableOpacity>
                 </View>
             </View>
-               <Text style={styles.ContactUs_header}>
+            <View style={styles.contact}>
+                <Text style={styles.ContactUs_header}>
                     Contact Us
-                     </Text>
-             <View style={styles.contactUs}>
-             
-               
-                <View style={styles.second_row}>
-                    <Text>
-                        <Entypo name='old-phone' size={30} color='white' />
-                        <Text style={styles.footer_text}>
-                            +802 547 9512
+                </Text>
+                <View style={styles.contactUs}>
+                    <View style={styles.second_row}>
+                        <Text>
+                            <Entypo name='old-phone' size={30} color='white' />
+                            <Text style={styles.footer_text}>
+                                +802 547 9512
+                            </Text>
                         </Text>
-                    </Text>
-                    <Text>
-                        <FontAwesome name='whatsapp' size={30} color='white' />
-                        <Text style={styles.footer_text}>
-                            +854 712 1369
+                        <Text>
+                            <FontAwesome name='whatsapp' size={30} color='white' />
+                            <Text style={styles.footer_text}>
+                                +854 712 1369
+                            </Text>
                         </Text>
-                    </Text>
-                    <Text>
-                        <Entypo name='email' size={30} color='white' />
-                        <Text style={styles.footer_text}>
-                            instaloan@biz.com
+                        <Text>
+                            <Entypo name='email' size={30} color='white' />
+                            <Text style={styles.footer_text}>
+                                instaloan@biz.com
+                            </Text>
                         </Text>
-                    </Text>
+                    </View>
                 </View>
-            </View>  
+            </View>
         </View>
     );
 }
@@ -81,52 +81,45 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#0e2496',
-
     },
     follow_text: {
         fontSize: 15,
         color: 'white',
         marginTop: 30,
-       
     },
     followUs: {
         flex: 10,
-
     },
     Follow: {
         marginHorizontal: 10,
         marginVertical: 25,
         backgroundColor: '#364bba',
-        borderRadius:15,
+        borderRadius: 15,
         height: 200,
         width: 200,
-         alignItems:'center',
-        justifyContent:'center'
+        alignItems: 'center',
+        justifyContent: 'center'
 
     },
-    fb: { 
-        left: -10
-
-    },
-    twitter:{
-        left: -19,
-       
-    },
-    Insta:{
+    fb: {
         left: -10,
-        top:-70
     },
-    linkedin:{
+    twitter: {
         left: -19,
-        top:-70
-        
+    },
+    Insta: {
+        left: -10,
+        top: -70
+    },
+    linkedin: {
+        left: -19,
+        top: -70
     },
     row1: {
         flexDirection: 'row',
         marginVertical: 30,
-        
-    },
 
+    },
     row2: {
         flexDirection: 'row'
 
@@ -135,51 +128,37 @@ const styles = StyleSheet.create({
         marginVertical: 40,
         marginLeft: 23
     },
-
     followUs_header: {
         fontSize: 30,
         fontWeight: 'bold',
         color: 'white',
-        marginVertical:40,
-       marginHorizontal:20,
-       alignItems:'center',
-       justifyContent:'center'
+        marginVertical: 40,
+        marginHorizontal: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        top: 40
     },
-
     contactUs: {
         marginHorizontal: 10,
         borderRadius: 30,
         backgroundColor: '#364bba',
-        marginBottom: 30
-
-
+        marginBottom: 30,
     },
-
     ContactUs_header: {
-        fontSize: 20,
+        fontSize: 30,
         fontWeight: 'bold',
         color: 'white',
-
-
+        marginHorizontal: 20,
+        marginVertical: 10
     },
-
-
-
-
-
+    contact: {
+        bottom: 79
+    },
     footer_text: {
 
         color: 'white',
+        marginLeft: 30,
+        fontSize:30
     },
-
-
-
-
-
-
-
-    // insta:{
-    //     flex:2
-    // }
 
 })
