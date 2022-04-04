ea2783dc4f08faa037c66651cd3ce977eb7b314a
kencal20 <kenadjei20@gmail.com>
2022-04-04 08:03:36 +0000

    loan update

--- src/Loan.js
+++ src/Loan.js
@@ -12,18 +12,19 @@ const Loan = ({ navigation }) => {
                 Types of loan
             </Text>
             <View style={styles.first_row}>
-                <TouchableOpacity
-                    onPress={() => {
-                        navigation.navigate("Studentloan")
-                    }}
-                    style={styles.loantype_button}
-                >
-                    <Text style={styles.button_text}>
-                        Students loan
-                    </Text>
-                </TouchableOpacity>
-
-                <View>
+                <View style={styles.for_students}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate("Studentloan")
+                        }}
+                        style={styles.loantype_button}
+                    >
+                        <Text style={styles.button_text}>
+                            Students loan
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.for_workers}>
                     <TouchableOpacity
                         onPress={() => {
                             navigation.navigate("Workersloan")
@@ -35,87 +36,22 @@ const Loan = ({ navigation }) => {
                     </TouchableOpacity>
 
                 </View>
-                <View style={styles.for_pension}>
-                    <TouchableOpacity
-                        onPress={() => {
-                            navigation.navigate("Pensionloan")
-                        }}
-                        style={styles.loantype_button}
-                    >
-                        <Text style={styles.button_text}>
-                            Pension  loan
-                        </Text>
-                    </TouchableOpacity>
-                </View>
             </View>
-            <View style={styles.footer}>
-                <View style={styles.followUs}>
+            <View style={styles.for_pension}>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate("Pensionloan")
+                    }}
+                    style={styles.loantype_button}
+                >
+                    <Text style={styles.button_text}>
+                        Pension  loan
+                    </Text>
+                </TouchableOpacity>
 
-                    <Text style={styles.followUs_header}>
-                        Follow us
-                    </Text>
-                    <View style={styles.followus_row}  >
-                        <View>
-                        <Text >
-                            <TouchableOpacity style={styles.fb}>
-                                <Entypo name='facebook' size={30} color='white' />
-                            </TouchableOpacity>
-                            <Text style={styles.footer_text}>
-                                facebook
 
-                            </Text>
-                        </Text>
-                        </View>
-                    
-                    <View>
-                        <Text >
-                            <TouchableOpacity style={styles.twitter_button}>
-                                <Entypo name='twitter' size={30} color='white' />
-
-                            </TouchableOpacity>
-                            <Text style={styles.footer_text}>
-                                twitter
-                            </Text>
-                        </Text>
-                    </View>
-                    <View>
-                        <Text >
-                            <TouchableOpacity style={styles.insta_button}>
-                                <Entypo name='instagram' size={30}  color='white'/>
-                            </TouchableOpacity>
-                            <Text style={styles.footer_text}>
-                                instagram
-                            </Text>
-                        </Text>
-                    </View>
-                </View>
-                </View>
-                <View style={styles.contactUs}>
-                    <Text style={styles.ContactUs_header}>
-                        Contact Us
-                    </Text>
-                    <View style={styles.second_row}>
-                    <Text>
-                        <Entypo name='old-phone' size={30}  color='white'/>
-                        <Text style={styles.footer_text}>
-                            +802 547 9512
-                        </Text>
-                    </Text>
-                    <Text>
-                        <FontAwesome name='whatsapp' size={30} color='white' />
-                        <Text style={styles.footer_text}>
-                            +854 712 1369
-                        </Text>
-                    </Text>
-                    {/* <Text>
-                        <Entypo name='email' size={30} color='white' />
-                        <Text style={styles.footer_text}>
-                            instaloan@biz.com
-                        </Text>
-                    </Text> */}
-                </View>
-                </View>
             </View>
+
         </View>
 
     );
@@ -126,7 +62,6 @@ export default Loan;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         backgroundColor: '#0e2496',
 
     },
@@ -135,7 +70,8 @@ const styles = StyleSheet.create({
         fontSize: 40,
         fontWeight: 'bold',
         color: 'white',
-        marginHorizontal:40
+        marginHorizontal: 40,
+        marginVertical: 50
 
 
     },
@@ -148,28 +84,31 @@ const styles = StyleSheet.create({
     loantype_button: {
         borderWidth: 0,
         borderRadius: 15,
-        marginVertical: 20,
         marginHorizontal: 20,
-        height: 100,
-        width: 100,
+        height: 200,
+        width: 200,
         backgroundColor: '#364bba',
         justifyContent: 'center',
         alignItems: 'center',
+        left: -20
 
 
 
     },
     first_row: {
         flexDirection: 'row',
-        marginVertical:90
+        marginVertical: 90
     },
     for_students: {
-        flex: 5
+
 
     },
     for_workers: {
         flex: 5,
-
+        left: -30
+    },
+    for_pension: {
+        top: -80
     },
 
 
@@ -178,52 +117,7 @@ const styles = StyleSheet.create({
         color: 'white'
     },
 
-    footer_text: {
-        fontSize: 15,
-        color:'white',
-        marginTop:30
-    },
 
-    followUs: {
-        marginHorizontal: 10,
-        marginBottom: 20,
-       flexDirection:'row',
-        marginVertical: 10,
-        backgroundColor: '#364bba',
-        borderRadius:30
-    },
-
-    followUs_header: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color:'white'
-    },
-    followus_row:{
-        marginVertical:30,
-        flexDirection:'row',
-        marginRight:50
-    },
-    contactUs: {
-        marginHorizontal: 10,
-       borderRadius:30,
-        backgroundColor: '#364bba',
-        marginBottom:30
-     
-       
-    },
-
-    ContactUs_header: {
-        fontSize: 20,
-        fontWeight: 'bold',
-         color:'white',
-         flexDirection:'row'
-    },
-    second_row:{
-        flexDirection: 'row',
-        marginHorizontal:90,
-
-        
-    }
 
 
 
