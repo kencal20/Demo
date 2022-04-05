12d795818de586d51fa015cfd6eff598b1c219bc
kencal20 <kenadjei20@gmail.com>
2022-04-05 10:10:56 +0000

    banks js update

--- src/Bank.js
+++ src/Bank.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import {View,Text} from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import scb from '../assets/Scb.png'
+import fidelity from '../assets/Fidelity.jpg'
+import absa from '../assets/Absa.png'
+import ecobank from '../assets/Ecobank.png'
+import { } from 'react-native-web';
 const Bank = () => {
     return (
-    <View>
-<Text>
-    Bank
-</Text>
-    </View>
+        <View>
+            <TouchableOpacity>
+                <Image source={scb} style={styles.img} />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Image source={fidelity} style={styles.img} />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Image source={absa} style={styles.img} />
+            </TouchableOpacity>
+            
+            <TouchableOpacity>
+                <Image source={ecobank} style={styles.img} />
+            </TouchableOpacity>
+            <Text>
+                Bank
+            </Text>
+        </View>
     );
 }
 
 export default Bank;
+
+const styles = StyleSheet.create({
+
+    img: {
+        width: 300,
+        marginVertical: 10,
+        marginHorizontal: 20
+
+    }
+
+})
